c3afdad3678c01eb12601de7cfa3f661464ce3b8
Gladius Regis <gladiusregis@gmail.com>
2022-04-10 22:03:59 +0200

    LIST COMPREHENSION introduction

---
+++ LIST_COMPREHENSION_choosing_tuples.py
@@ -0,0 +1,13 @@
+tuples = [
+    (1,),  # (1,) po elemencie MUSI BYĆ PRZECINEK !!! INACZEJ TO NIE JEST TUPLE
+    (2, 3),
+    (3, 4),
+    (4, 3, 4),
+    (1, 2, 3, 4),
+    (2, 3, 4, 5),
+    (2, 2, 2),
+    (1, 1, 1, 5, 4, 3, 4)
+]
+
+result = [ sum(item) if len(item) % 2 == 0 else round(sum(item) / len(item), 2) for item in tuples if 1 < len(item) < 6]
+print(result)
