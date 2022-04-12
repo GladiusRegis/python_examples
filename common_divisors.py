c29464032367a3ba264dcda33554a1096e803803
Gladius Regis <gladiusregis@gmail.com>
2022-04-12 22:10:08 +0200

    division into two functions - divisors, offset

--- common_divisors.py
+++ common_divisors.py
@@ -1,11 +1,9 @@
-def calculate_common_divisor(number_1, number_2, offset):
-
-    set_1 = {number for number in range(2, number_1) if number_1 % number == 0}
-    set_2 = {number for number in range(2, number_2) if number_2 % number == 0}
-
-    bigger_than = [bigger for bigger in (set_1 & set_2) if bigger > offset]
-
-    return bigger_than
+def find_divisors(number):
+   divisors = set()
+   for divisor in range(2, number + 1)
+       if number % divisor == 0:
+           divisors.add(divisor)
+   return divisors
 
 
 test1 = calculate_common_divisor(3, 6)
