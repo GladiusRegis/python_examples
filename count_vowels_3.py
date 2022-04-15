66c542ba0d3a838256bdb64db07c01bbe881b01e
Gladius Regis <gladiusregis@gmail.com>
2022-04-15 21:21:11 +0200

    first: bool, two: int

--- count_vowels_3.py
+++ count_vowels_3.py
@@ -1,7 +1,17 @@
 def count_vowels(text):
     return sum([char in 'aeouy' for char in text])
+    # returns true false
+
+
+# Kacper way
+def count_vowels2(text):
+    return sum([1 if char in 'aeouy' else 0 for char in text])
 
 
 print(count_vowels('ala'))
+print(count_vowels('lll'))
+print(count_vowels('programing'))
 
-print(count_vowels('programowanie'))
+print(count_vowels2('ala'))
+print(count_vowels2('lll'))
+print(count_vowels2('programing'))
