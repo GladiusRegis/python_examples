1283ea074a53e7d4767d995185b612b0257039f9
Gladius Regis <gladiusregis@gmail.com>
2022-04-15 23:06:45 +0200

    division into two functions with single functionality

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -1,10 +1,11 @@
-def give_greatest_divisor(number1: int, number2: int) -> int:
-    set_1 = {number for number in range(2, number1 + 1) if number1 % number == 0}
-    set_2 = {number for number in range(2, number2 + 1) if number2 % number == 0}
-    list_common = list(set_1 & set_2)
-
-    divisor = 0 if list_common == [] else max(list_common)
-    return divisor
+def find_divisors(a: int) -> set:
+    return {d for d in range(1, a + 1) if a % d == 0}
 
 
-print(give_greatest_divisor(15, 21))
+def find_greatest_divisor(a: int, b: int) -> int:
+    div_a = find_divisors(a)
+    div_b = find_divisors(b)
+    return max(div_a & div_b)
+
+
+print(find_greatest_divisor(32, 48))
