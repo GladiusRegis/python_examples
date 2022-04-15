8fa40e769b076075b0f58e5d8da70c0d00807f11
Gladius Regis <gladiusregis@gmail.com>
2022-04-15 13:56:15 +0200

    first version

---
+++ list_zip_3.py
@@ -0,0 +1,11 @@
+def get_sum(list1: list, list2: list) -> list:
+    total = []
+    for a, b in zip(list1, list2):
+        total.append(a + b)
+
+    return total
+
+list_one = [2, 4, 6]
+list_two = [8, 6, 4]
+
+print(get_sum(list_one, list_two))
