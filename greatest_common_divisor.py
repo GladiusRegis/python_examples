978813f105edd89f55bed916b2ff23aa9975ea22
Gladius Regis <gladiusregis@gmail.com>
2022-04-15 23:09:21 +0200

    added Algorytm Euklidesa

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -8,4 +8,27 @@ def find_greatest_divisor(a: int, b: int) -> int:
     return max(div_a & div_b)
 
 
-print(find_greatest_divisor(32, 48))
+"""Algorytm Euklidesa"""
+
+
+def algorithm_euclidean(number1: int, number2: int) -> int:
+    # faster method for large numbers
+    if number1 < number2:
+        x = number1
+        number1 = number2
+        number2 = x
+
+    done = False
+    while not done:
+        rest = number1 % number2
+        if rest == 0:
+            done = True
+            continue
+        number1 = number2
+        number2 = rest
+    return number2
+
+
+print(find_greatest_divisor(96, 16))
+print(algorithm_euclidean(96686800008, 16857979797972444))
+
