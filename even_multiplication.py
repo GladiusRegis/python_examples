6fce8c30ddcf33d2b625b243fd6b3266e522fff1
Gladius Regis <gladiusregis@gmail.com>
2022-04-15 15:44:10 +0200

    added while not dome

--- even_multiplication.py
+++ even_multiplication.py
@@ -1,11 +1,15 @@
-value = input('Enter a number or enter [exit]: ')
+done = False
+number_list = []
 summary = 1
-while value != 'exit':
-    number = int(value)
-
-    if number % 2 == 0:
-        summary *= number
-
-    value = input('Enter a number or enter [exit]: ')
-
-print(f'Product of even values is {summary}')
+while not done:
+    number = input('Enter a number or enter [exit]: ')
+    if number == 'exit':
+        done = True
+    else:
+        number = int(number)
+        if number % 2 == 0:
+            number_list.append(number)
+length = len(number_list)
+for i in range(0, length):
+    summary *= number_list[i]
+print(summary)
