33e96141f877b37886dbca55cacd404ea8a72e8e
Gladius Regis <gladiusregis@gmail.com>
2022-04-22 16:01:58 +0200

    args kwargs exemple

---
+++ args_kwargs_4w.py
@@ -0,0 +1,10 @@
+
+def group_them(**kwargs):
+    for key, value in kwargs.items():
+        print(f'{key.capitalize()} is {value}')
+
+
+def test_group_them(capsys):
+    group_them(wall='red', tomato='red', light='yellow')
+    out, err = capsys.readouterr()
+    assert out == 'Wall is red\nTomato is red\nLight is yellow\n'
