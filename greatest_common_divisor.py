7ea51b3c8c894e6b9fe1e2751459b72df249fc7e
Gladius Regis <gladiusregis@gmail.com>
2022-04-23 10:25:33 +0200

    program test

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -13,10 +13,10 @@ def find_greatest_divisor(a: int, b: int) -> int:
 
 def algorithm_euclidean(number1: int, number2: int) -> int:
     # faster method for large numbers
-    if number1 < number2:
-        x = number1
-        number1 = number2
-        number2 = x
+    # if number1 < number2:
+    #     x = number1
+    #     number1 = number2
+    #     number2 = x
 
     done = False
     while not done:
@@ -24,11 +24,10 @@ def algorithm_euclidean(number1: int, number2: int) -> int:
         if rest == 0:
             done = True
             continue
-        number1 = number2
-        number2 = rest
+        number1, number2 = number2, rest
     return number2
 
 
 print(find_greatest_divisor(96, 16))
-print(algorithm_euclidean(96686800008, 16857979797972444))
+print(algorithm_euclidean(8, 4))
 
