918ccd6c7c8347514b7b081a6133ccbd3caefc08
Gladius Regis <gladiusregis@gmail.com>
2022-04-26 11:02:04 +0200

    pure code module csv

--- text_income_filter_module_csv.py
+++ text_income_filter_module_csv.py
@@ -4,23 +4,20 @@ from csv import reader, writer
 
 result = []
 with open('transactions.csv', encoding='utf8') as input_file:
-    content = reader(input_file, delimiter=',')  # delimiter - separator
-    next(content)  # przejście do kolejnego wiersza - nie potrzebujemy nagłówka
-    for line in content:  # teraz iterujemy content a nie input_file
-        # print(line.strip())  # (1)
-        #  print(line)  # (2)
-        # są przecinki można zrobić split, ale lepiej skorzystać z biblioteki csv
-        created_at, description, value = line  # rozpakowanie
-        # print(int(value))  # (3)
+    content = reader(input_file, delimiter=',')
+    next(content)
+
+    #  for line in input_file:
+        #  print(line.strip())
+    for line in content:
+        #  print(line)
+        created_at, description, value = line
+
         if int(value) > 0:
             result.append(line)
-            # można i tu zapisywać za każdym razem do pliku za pomocą a
-            # with open(.... , 'a')
-            #      write.
-            # za każdym razem trzeba otwierać i zamykać plik
-            # można zrobić z użyciem generatora i zwracać każdą pojedyńcząl linę
 
-with open('income.csv', 'w', encoding='utf8', newline='') as output_file:  # newline - aby nie było pustej linie
+
+with open('income.csv', 'w', encoding='utf8', newline='') as output_file:
     content = writer(output_file, delimiter=',')
     for line in result:
         content.writerow(line)
