5f1e4568d49c0a4289612fe9137803274980a029
Gladius Regis <gladiusregis@gmail.com>
2022-05-03 17:38:31 +0200

    correcting the code for argument 0 or 1

--- get_fibonacci_3.py
+++ get_fibonacci_3.py
@@ -1,11 +1,15 @@
 def get_fibonacci(limit):
+    # if limit == 0:
+    #     return[]
+    # if limit == 1:
+    #     return[0]
     a = 0
     b = 1
     result = [a, b]
     while len(result) < limit:
         a, b = b, a + b
         result.append(b)
-    return result
+    return result[:limit]
 
 
 print(get_fibonacci(0))
