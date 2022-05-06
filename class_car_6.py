eeb69cb2704af12711a4b28cbb48cf0d5a853687
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 11:56:45 +0200

    clean code, added new class Collecton with method

--- class_car_6.py
+++ class_car_6.py
@@ -8,19 +8,31 @@ class Car:
         return f'{self.name}, price: {self.price}, max speed {self.speed}'
 
 
+class Collection:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def get_items(self, key, reverse=False):
+        return sorted(self.items, key=key, reverse=reverse)
+
+
 if __name__ == '__main__':
-    cars = []
-    for _ in range(2):
+    collection = Collection()
+
+    for _ in range(5):
         name_user = input('Name: ')
         price_user = input('Price: ')
         speed_user = input('Max speed: ')
-        cars.append(Car(name_user, price_user, speed_user))
+        collection.add_item(Car(name_user, price_user, speed_user))
         print('--' * 10)
-
-    for car in sorted(cars, key=lambda c: c.price, reverse=True):
+    print('Sorted by price:')
+    for car in collection.get_items(key=lambda c: c.price, reverse=True):
         print(car.get_info())
-
-    for car in sorted(cars, key=lambda c: c.speed):
+    print('Sorted by speed')
+    for car in collection.get_items(key=lambda c: c.speed):
         print(car.get_info())
 
 
