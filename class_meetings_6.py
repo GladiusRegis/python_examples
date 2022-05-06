a77b96fd8ebdf87f25bfe68bf0e310e547a7b704
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 20:07:13 +0200

    added method next available slot

--- class_meetings_6.py
+++ class_meetings_6.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Meeting:
@@ -11,9 +11,20 @@ class Calendar:
     def __init__(self):
         self.meetings = {}
 
+    def is_available(self, date: datetime):
+        return date not in self.meetings
+
     def add_meeting(self, meeting: Meeting):
-        if meeting.date not in self.meetings:
+        if self.is_available(meeting.date):
             self.meetings[meeting.date] = meeting
+
+    def next_available_slot(self, date: datetime):
+        meeting_date = date
+        while not self.is_available(meeting_date):
+            meeting_date += timedelta(minutes=60)
+        return meeting_date
+
+
 
 
 def test_add_meeting():
