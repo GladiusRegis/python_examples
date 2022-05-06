b4e046c20a762646238fbd160e9da80c14361c87
Gladius Regis <gladiusregis@gmail.com>
2022-05-06 20:23:44 +0200

    test check nect available slot

--- class_meetings_6.py
+++ class_meetings_6.py
@@ -25,6 +25,32 @@ class Calendar:
         return meeting_date
 
 
+def test_check_next_available_slot():
+    # given
+    birthday = Meeting(datetime(2020, 11, 9, 12, 0), 'My birthday!')
+    birthday2 = Meeting(datetime(2020, 11, 9, 13, 0), 'My birthday!')
+    birthday3 = Meeting(datetime(2020, 11, 9, 14, 0), 'My birthday!')
+    calendar = Calendar()
+    calendar.add_meeting(birthday)
+    calendar.add_meeting(birthday2)
+    calendar.add_meeting(birthday3)
+
+    # when
+    next_time_slot = calendar.next_available_slot(datetime(2020, 11, 9, 12, 0))
+
+    # then
+    assert next_time_slot == datetime(2020, 11, 9, 15, 0)
+
+
+def test_is_given_datetime_available():
+    # given
+    calendar = Calendar()
+
+    # when
+    next_time_slot = calendar.next_available_slot(datetime(2020, 11, 9, 12, 0))
+
+    # then
+    assert next_time_slot == datetime(2020, 11, 9, 12, 0)
 
 
 def test_add_meeting():
