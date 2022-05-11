f9f7fcb1ebb99adaa4b9883b02bbca633dc50429
Gladius Regis <gladiusregis@gmail.com>
2022-05-11 23:19:39 +0200

    improved implementation of methods from the abstract class

--- class_abstractions_6.py
+++ class_abstractions_6.py
@@ -2,12 +2,16 @@ from abc import ABC, abstractmethod
 
 
 class Item(ABC):
-    def __init__(self, name):
+    def __init__(self, name, price):
+        self.price = price
         self.name = name
 
     @abstractmethod
-    def get_name(self):
+    def get_total_price(self):
         pass
 
-a = Item('ABC')
-# TypeError: Can't instantiate abstract class Item with abstract method get_name
+
+class Product(Item):
+    def get_total_price(self):
+        return self.price
+
